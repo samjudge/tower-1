b2ea343e4869ce7f4b981f052005428f00068a91
samjudge <sam.m.w.judge@googlemail.com>
2017-06-16 17:23:29 +0100

    added chests

---
+++ Assets/Scripts/Items/PickupFactory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+using System;
+
+public class PickupFactory : MonoBehaviour {
+
+    public ItemFactory ItemFactory;
+    public Hand Hand;
+
+    //Weapons
+    public Pickup Sword;
+
+    //Items
+    public Pickup Key;
+
+    public Pickup MakePickup(string name) {
+        FieldInfo Property = this.GetType().GetField(name);
+        Pickup PickupPrefab = Property.GetValue(this) as Pickup;
+        Pickup nPickup = Instantiate(PickupPrefab, new Vector3(0, 0, 0), Quaternion.Euler(15, 180, 0)) as Pickup;
+        nPickup.Name = name;
+        nPickup.ItemFactory = ItemFactory;
+        nPickup.Hand = Hand;
+        return nPickup;
+    }
+}
---
+++ Assets/Scripts/Prefabs/Chest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour {
+
+    public string Contains;
+    public PickupFactory PickupFactory;
+    public Player Player;
+    bool isOpended = false;
+
+    void OnMouseDown(){
+        if ((this.transform.position - Player.transform.position).sqrMagnitude <= 1){
+            if (!isOpended){
+                isOpended = !isOpended;
+                StartCoroutine(Open());
+            }
+        }
+    }
+
+    private IEnumerator Open() {
+        Player.ActionLog.WriteNewLine("you open the chest");
+        Pickup p = this.PickupFactory.MakePickup(Contains);
+        p.transform.position = this.transform.position;
+        MonoBehaviour.Destroy(this.gameObject); //destroy this chest
+        yield return null;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        //check if player
+        Player p = other.gameObject.GetComponent<Player>() as Player;
+        if (p != null)
+        {
+            //set flag to cancel movement and return
+            p.SetCancelMovementFlag(true);
+        }
+    }
+}
