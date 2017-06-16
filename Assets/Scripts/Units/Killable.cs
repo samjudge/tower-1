c4e3962d011afb8049498a6a0ec50bbf153da09e
samjudge <sam.m.w.judge@googlemail.com>
2017-06-16 14:08:20 +0100

    missing files

---
+++ Assets/Scripts/Units/Killable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Killable : MonoBehaviour {
+
+    public void DestroyParent() {
+        MonoBehaviour.Destroy(this.transform.parent.gameObject); //destroy on death
+    }
+}
