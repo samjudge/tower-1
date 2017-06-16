6751b887e463959321cd3f10029d8cd6c6ba6ad2
samjudge <sam.m.w.judge@googlemail.com>
2017-06-16 12:23:04 +0100

    refactored equipment sys

--- Assets/Scripts/UI/EquipmentSlot.cs
+++ Assets/Scripts/UI/EquipmentSlot.cs
@@ -8,54 +8,31 @@ public class EquipmentSlot : ItemSlot
     public string SlotName;
     public Equipment DefaultItem;
 
-    override public void OnPointerClick(PointerEventData e) {
-        if (this.Item != null) {
-            if ((this.Item.GetComponent<Item>().GetType()).IsSubclassOf(typeof(Equipment))) { //if it's a weapon
-                Debug.Log("Is Equipment in slot");
-                Equipment Equipment = this.Item.GetComponent<Equipment>() as Equipment;
-                if (CanEquipTo(Equipment,this.SlotName)) { //if the slot is valid
-                    Debug.Log("Can equip");
-                    GameObject held = Hand.GetHeldAndRemoveAsChild();
-                    if (held != null) { //if your hand isn't empty
-                        Debug.Log("hand is NOT empty");
-                        //unequip the item that was in this slot
-                        GameObject was = this.GetItemAndDetatch();
-                        //equp the item in hand
-                        this.SetItemAndMakeChild(held);
-                        Equipment = this.Item.GetComponent<Equipment>() as Equipment;
-                        this.Player.Equipment[this.SlotName] = Equipment;
-                        //set your hand to hold the item that was in this slot
-                        this.Hand.SetHeld(was);
-                    } else {
-                        Debug.Log("hand IS empty");
-                        //set the item in this slot to be null
-                        GameObject was = this.GetItemAndDetatch();
-                        this.Hand.SetHeld(was);
-                        //set player's item to the deault (fist etc)
-                        this.Player.Equipment[this.SlotName] = DefaultItem;
-                    }
-                }
-            } else {//if it's something else
-                //don't equip
-            }
+    private void SwapItemWithHeld() {
+        GameObject held = Hand.GetHeld();
+        GameObject was = GetItemAndDetatch();
+        if (held == null) {
+            Hand.SetHeld(was);
+            //SetItemAndMakeChild(was);
+            return;
         }
-        else{
-            Debug.Log("NO item in slot");
-            GameObject held = Hand.GetHeld();
-            if (held != null) {
-                if ((held.GetComponent<Item>().GetType()).IsSubclassOf(typeof(Equipment))) { //if the item in your hand is an equipment
-                    if (CanEquipTo(held.GetComponent<Item>(), this.SlotName)) {
-                        held = Hand.GetHeldAndRemoveAsChild();
-                        this.SetItemAndMakeChild(held);
-                    }
-                } else {
-                    //don't equip
-                }
-            }
+        if (CanEquipTo(held, this.SlotName)) {
+            Debug.Log("can equip");
+            Hand.GetHeldAndRemoveAsChild();
+            
+            SetItemAndMakeChild(held);
+            Player.Equipment[this.SlotName] = held.GetComponent<Equipment>() as Equipment;
+        } else {
+            Debug.Log("cannot equip");
+            SetItemAndMakeChild(was);
         }
     }
+    
+    override public void OnPointerClick(PointerEventData e) {
+        SwapItemWithHeld();
+    }
 
-    private bool CanEquipTo(Item i, string avail){
+    private bool CanEquipTo(GameObject i, string avail){
         if (i.GetComponent<Item>().GetType().IsSubclassOf(typeof(Equipment))) {
             Equipment e = i.GetComponent<Equipment>() as Equipment;
             foreach(string t in e.EquippableTo) {
