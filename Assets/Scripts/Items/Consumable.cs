fd78702a3b3fd1b7aaa54a42dcfc9cab16a4a00d
samjudge <sam.m.w.judge@googlemail.com>
2017-06-18 16:12:59 +0100

    missing files

---
+++ Assets/Scripts/Items/Consumable.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Consumable : Item {
+    abstract public void ConsumeEffectOn(Unit u);
+}
---
+++ Assets/Scripts/Items/HealthPotion.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class HealthPotion : Consumable{
+    public float RestoreXHP;
+
+    override public void ConsumeEffectOn(Unit u) {
+        if (u.Hp + RestoreXHP > u.CalculateMaxHp()) {
+            u.Hp = u.CalculateMaxHp();
+        } else {
+            u.Hp += RestoreXHP;
+        }
+        if (u.GetType() == typeof(Player)) {
+            Player p = u as Player;
+            p.HPBar.UpdateBar(p.Hp,p.CalculateMaxHp());
+        }
+    }
+}
